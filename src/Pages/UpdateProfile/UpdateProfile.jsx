65e599e179798ab18284ba489ed76f1f33d903d0
Rocky Dey <coderrocky25@gmail.com>
2024-04-07 22:14:29 +0600

    all pages created with added to route

---
+++ src/Pages/UpdateProfile/UpdateProfile.jsx
@@ -0,0 +1,9 @@
+const UpdateProfile = () => {
+  return (
+    <div>
+      <h1>This is Update Profile Page</h1>
+    </div>
+  );
+};
+
+export default UpdateProfile;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,15 +1,42 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../Pages/Home/Home";
+import Blogs from "../Pages/Blogs/Blogs";
+import Contact from "../Pages/Contact/Contact";
+import UpdateProfile from "../Pages/UpdateProfile/UpdateProfile";
+import Login from "../Pages/Login/Login";
+import Register from "../Pages/Register/Register";
+import NotFound from "../Pages/NotFound/NotFound";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/",
         element: <Home />,
+      },
+      {
+        path: "/blogs",
+        element: <Blogs />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/updateProfile",
+        element: <UpdateProfile />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
       },
     ],
   },
