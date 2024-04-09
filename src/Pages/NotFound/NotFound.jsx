c5dcba78c76ba65a6c6c4c8c2fb4f4162736e99e
Rocky Dey <coderrocky25@gmail.com>
2024-04-09 19:07:47 +0600

    404 not found page complete

--- src/Pages/NotFound/NotFound.jsx
+++ src/Pages/NotFound/NotFound.jsx
@@ -1,7 +1,15 @@
+import notFoundImg from "../../assets/images/404.jpg";
+import Footer from "../../components/Shared/Footer/Footer";
+import Header from "../../components/Shared/Header/Header";
+
 const NotFound = () => {
   return (
     <div>
-      <h1>404! Not Found</h1>
+      <Header />
+      <div className='max-w-screen-xl mx-auto flex justify-center items-center'>
+        <img className='w-3/5 mt-20 lg:mt-0' src={notFoundImg} alt='' />
+      </div>
+      <Footer />
     </div>
   );
 };
